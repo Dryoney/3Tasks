858f146e8ff115819292151def96caa2a1da7f3b
dryoney <74609294+Dryoney@users.noreply.github.com>
2025-05-27 20:52:13 +0500

    Fixes of comments

--- task_1/main.py
+++ task_1/main.py
@@ -1,35 +1,46 @@
+LOG_FILE_NAME = 'log.txt'
+
+
 def count_lines():
-    with open('log.txt','rt') as log:
-        lines = log.readlines()
-        print(str(len(lines))+": количество строк")
-def count_errors():
-    with open('log.txt','rt') as log:
-        counter_errors = 0
+    with open(LOG_FILE_NAME, 'rt') as file:
+        return len(file.readlines())
+
+
+def count_topic(topic):
+    topic_counter = 0
+    with open(LOG_FILE_NAME, 'rt') as file:
         while True:
-            line = log.readline()
+            line = file.readline()
             if not line:
                 break
-            if line.find('ERROR') != -1:
-                counter_errors += 1
-        print('Количество ошибок:'+str(counter_errors))
+            if line.find(topic) != -1:
+                topic_counter += 1
+    return topic_counter
+
+
 def find_users():
     logged_users = set()
-    with open('log.txt','rt') as file:
+    with open(LOG_FILE_NAME, 'rt') as file:
         for line in file:
-            if "logged in" in line:  # Проверяем наличие фразы в строке
-                parts = line.split()  # Разделяем строку на части
+            if 'logged in' in line.lower():  # Проверяем наличие фразы в строке.
+                parts = line.split()  # Разделяем строку на части.
                 username = parts[4]
                 logged_users.add(username)
     return logged_users
+
+
 def main():
     try:
-        open('log.txt', 'rt')
+        open(LOG_FILE_NAME, 'rt')
     except FileNotFoundError:
         print('Файл не найден')
-    count_lines()
-    count_errors()
+    topic = 'ERROR'
+    print(f'Количество строк: {count_lines()}')
+    print(f'Количество {topic}(s) {count_topic(topic)}')
     users = find_users()
     for user in users:
         print(user)
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     main()
