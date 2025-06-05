7968f951994eb175a1e94776c80ffc8ee296a9f6
dryoney <74609294+Dryoney@users.noreply.github.com>
2025-06-05 18:07:44 +0500

    Fixes

--- task_1/main.py
+++ task_1/main.py
@@ -1,45 +1,51 @@
+import os
+from collections import defaultdict
+from typing import Literal
+
 LOG_FILE_NAME = 'log.txt'
 
 
-def count_lines():
+def get_logs_count() -> int:
     with open(LOG_FILE_NAME, 'rt') as file:
         return len(file.readlines())
 
-
-def count_topic(topic):
-    topic_counter = 0
+Topics = Literal ['INFO','ERROR','WARNING']
+def get_topic_count(topic: Topics) -> int:
+    topic_counter = defaultdict(int)
     with open(LOG_FILE_NAME, 'rt') as file:
         while True:
             line = file.readline()
             if not line:
                 break
             if line.find(topic) != -1:
-                topic_counter += 1
-    return topic_counter
+                topic_counter[topic] += 1
+    return topic_counter[topic]
 
 
-def find_users():
+def find_users() -> set:
     logged_users = set()
     with open(LOG_FILE_NAME, 'rt') as file:
         for line in file:
-            if 'logged in' in line.lower():  # Проверяем наличие фразы в строке.
-                parts = line.split()  # Разделяем строку на части.
+            if 'logged in' in line.lower():
+                parts = line.split()
                 username = parts[4]
                 logged_users.add(username)
     return logged_users
 
 
 def main():
-    try:
-        open(LOG_FILE_NAME, 'rt')
-    except FileNotFoundError:
+    if os.path.exists(LOG_FILE_NAME):
+        topic = 'ERROR'
+        logs_count = get_logs_count()
+        topic_count = get_topic_count(topic)
+        print(f'Количество строк: {logs_count}')
+        print(f'Количество {topic}(s) {topic_count}')
+        users = find_users()
+        for user in users:
+            print(user)
+    else:
         print('Файл не найден')
-    topic = 'ERROR'
-    print(f'Количество строк: {count_lines()}')
-    print(f'Количество {topic}(s) {count_topic(topic)}')
-    users = find_users()
-    for user in users:
-        print(user)
+
 
 
 if __name__ == '__main__':
